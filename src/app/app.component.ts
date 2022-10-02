49d0f39deeb3389ab55114121a2d3f4cdff4c311
rafabrunodev <rafaelbruno.k@hotmail.com>
2022-10-02 12:32:49 -0300

    Add Google Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import * as LogRocket from 'logrocket';
+import { environment } from "src/environments/environment";
+
+declare var window: any;
+declare var dataLayer: any;
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
 })
-export class AppComponent{
+export class AppComponent implements OnInit {
   onActivate(event) {
     window.scroll({
       top: 0,
       left: 0,
       behavior: 'auto'
     });
+  }
+
+  ngOnInit(): void {
+    if (environment.production) {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      //@ts-ignore
+      gtag('js', new Date());
+      //@ts-ignore
+      gtag('config', 'G-JBWNX0G3YF');
+    }
   }
 }
